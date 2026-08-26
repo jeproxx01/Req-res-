5aafe1103f84b9d4e23bac9a04dbcecc31a6d0cb
Jeffrey Cesar <cesar.jeffrey@hnu.edu.ph>
2026-08-26 21:55:12 +0800

    resolve api spec file

--- tests/api/GetBooking.spec.ts
+++ tests/api/GetBooking.spec.ts
@@ -1,11 +1,19 @@
  import { test, expect } from '@playwright/test';
 
-    test('Should retrieve booking details', async ({ request }) => {
-      // Playwright automatically sends this GET to https://restful-booker.herokuapp.com/booking/1
-      const response = await request.get('/booking/1');
+test('Should retrieve booking details', async ({ request }) => {
+  // Retrieve list of all bookings to get a valid, active booking ID
+  const listResponse = await request.get('/booking');
+  expect(listResponse.ok()).toBeTruthy();
+  
+  const bookings = await listResponse.json();
+  expect(bookings.length).toBeGreaterThan(0);
 
-      expect(response.ok()).toBeTruthy();
-      const body = await response.json();
-      console.log(body);
-      expect(body).toHaveProperty('firstname');
-    });
+  // Extract a dynamic booking ID
+  const bookingId = bookings[0].bookingid;
+  const response = await request.get(`/booking/${bookingId}`);
+
+  expect(response.ok()).toBeTruthy();
+  const body = await response.json();
+  console.log(body);
+  expect(body).toHaveProperty('firstname');
+});
